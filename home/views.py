98aec67143b49bc3f38b6c7220fd39b3f65e66bf
Carlos Cutile <carlos.cutile@outlook.com>
2024-06-11 02:03:28 -0300

    Se creo urls.py en Home

--- home/views.py
+++ home/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-from django.template import Template, context, loader
 
 def inicio(request):
     return HttpResponse('Bienvenidos a mi ProjectCoder')
 
-def template1(request):
-    return HttpResponse('Mi personalizacion')
+def template(request):
+    return render(request, 'template.html')
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from home.views import inicio, template1
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),
-    path('template1/', template1),
+    path('', include('incude.urls'))
 ]
